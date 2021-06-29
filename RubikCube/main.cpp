cf3afea3050b44adddbb21592466d2be5a75a4b8
Миша <308372@niuitmo.ru>
2021-06-29 19:06:50 +0300

    Initial Commit

---
+++ RubikCube/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RubikCube
+//
+//  Created by Я ноутбук on 29.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
